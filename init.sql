6d3619583fed4e13afdd130274ab276a4ec8c8ad
pedro <pedelgadillo@gmail.com>
2025-12-16 18:35:20 -0600

    chore: harden db - add non-superuser blog_app role with strong password and schema permissions

--- init.sql
+++ init.sql
@@ -12,7 +12,7 @@ BEGIN
   IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'blog_app') THEN
    CREATE ROLE blog_app WITH
     LOGIN
-    PASSWORD 'dev_app_password'
+    PASSWORD 'DevApp!2025@SecurePwd'
     NOSUPERUSER
     NOCREATEDB
     NOCREATEROLE
@@ -27,9 +27,16 @@ GRANT CONNECT ON DATABASE petedillo_blog TO blog_app;
 
 \c petedillo_blog
 
+-- Grant schema permissions
 GRANT USAGE ON SCHEMA public TO blog_app;
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO blog_app;
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO blog_app;
 
+-- Set default privileges for future tables
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO blog_app;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE, SELECT ON SEQUENCES TO blog_app;
+
+-- Additional: Grant schema usage to public for Flyway migrations (petedillo runs migrations)
+GRANT USAGE ON SCHEMA public TO blog_app;
+ALTER DEFAULT PRIVILEGES FOR USER petedillo IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO blog_app;
+ALTER DEFAULT PRIVILEGES FOR USER petedillo IN SCHEMA public GRANT USAGE, SELECT ON SEQUENCES TO blog_app;
